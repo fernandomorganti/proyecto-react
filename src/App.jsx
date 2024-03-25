637c375642b7e075394fd9c017afb4fae743c87b
Fernando Morganti <morgantifernando@gmail.com>
2024-03-25 13:00:34 -0300

    inicio proyecto

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Footer from "./Footer.jsx"
+function App() {
+
+  return (
+    <h1>Este es mi proyecto</h1>
+  )
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = ()=>{
+ return <h1>Aca va el footer</h1>
+}
+
+export default Footer
